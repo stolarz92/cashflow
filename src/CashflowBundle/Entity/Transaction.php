28a3a03d13d78f18f6ac8ea9236ce6d80a2f7903
Radoslaw Stolarski <stolarz92@gmail.com>
2016-05-31 12:58:31 +0200

    Add Wallet entity and Transaction entity with relationship mapping

---
+++ src/CashflowBundle/Entity/Transaction.php
@@ -0,0 +1,172 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stolarz
+ * Date: 31.05.16
+ * Time: 11:26
+ */
+
+namespace CashflowBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class Transaction
+ * @package CashflowBundle\Entity
+ * @ORM\Table(name="operations")
+ * @ORM\Entity
+ */
+class Transaction
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(
+     *     type="integer",
+     *     nullable=false,
+     *     options={
+     *         "unsigned"=true
+     *     }
+     * )
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(
+     *     name="name",
+     *     type="string",
+     *     length=128,
+     *     nullable=false
+     * )
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(
+     *     name="description",
+     *     type="string",
+     *     length=500,
+     *     nullable=true
+     * )
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(
+     *     name="amount",
+     *     type="decimal",
+     *     precision = 10,
+     *     scale = 2,
+     *     nullable=false
+     * )
+     */
+    private $amount;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Wallet", inversedBy="transactions")
+     * @ORM\JoinColumn(name="wallet_id", referencedColumnName="id")
+     **/
+    private $wallet;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Transaction
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Transaction
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set amount
+     *
+     * @param string $amount
+     * @return Transaction
+     */
+    public function setAmount($amount)
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    /**
+     * Get amount
+     *
+     * @return string 
+     */
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+    /**
+     * Set wallet
+     *
+     * @param \CashflowBundle\Entity\Wallet $wallet
+     * @return Transaction
+     */
+    public function setWallet(\CashflowBundle\Entity\Wallet $wallet = null)
+    {
+        $this->wallet = $wallet;
+
+        return $this;
+    }
+
+    /**
+     * Get wallet
+     *
+     * @return \CashflowBundle\Entity\Wallet 
+     */
+    public function getWallet()
+    {
+        return $this->wallet;
+    }
+}
