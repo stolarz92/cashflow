8b2b58a47d18b08d22e40dd4714215f0cdad1f49
Radoslaw Stolarski <stolarz92@gmail.com>
2016-06-01 12:38:41 +0200

    Add Wallet repository

---
+++ src/CashflowBundle/Repository/WalletRepository.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Wallet repository.
+ *
+ * @copyright (c) 2016 Tomasz Chojna
+ * @link http://epi.chojna.info.pl
+ */
+
+namespace CashflowBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Class Tag.
+ * @package AppBundle\Repository
+ * @author Tomasz Chojna
+ */
+class Wallet extends EntityRepository
+{
+
+}
