96ec1d5a664dc91d44d913716202c1f230fa7493
Radoslaw Stolarski <stolarz92@gmail.com>
2016-06-01 16:46:08 +0200

    Add wallet form

---
+++ src/CashflowBundle/Form/WalletType.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stolarz
+ * Date: 01.06.16
+ * Time: 13:40
+ */
+
+namespace CashflowBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * Class WalletType.
+ *
+ * @package AppBundle\Form
+ * @author Tomasz Chojna
+ */
+class WalletType extends AbstractType
+{
+    /**
+     * Form builder.
+     *
+     * @param FormBuilderInterface $builder Form builder
+     * @param array $options Form options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add(
+            'id',
+            'hidden'
+        );
+        $builder->add(
+            'name',
+            'text',
+            array(
+                'label' => 'Wallet name',
+                'required' => true,
+                'max_length' => 128,
+            )
+        );
+        $builder->add(
+            'description',
+            'text',
+            array(
+                'label' => 'Description',
+                'required' => false,
+                'max_length' => 500
+            )
+        );
+        $builder->add(
+            'save',
+            'submit',
+            array(
+                'label' => 'Save'
+            )
+        );
+    }
+
+    /**
+     * Sets default options for form.
+     *
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(
+            array(
+                'data_class' => 'CashflowBundle\Entity\Wallet'
+            )
+        );
+    }
+
+    /**
+     * Getter for form name.
+     *
+     * @return string Form name
+     */
+    public function getName()
+    {
+        return 'wallet_form';
+    }
+}
